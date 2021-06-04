67f9f379de0508a8d952245d1c848c9f2433dc78
dhartley <dani_hartley@msn.com>
2021-06-04 14:16:08 -0500

    updated

--- server.js
+++ server.js
@@ -42,6 +42,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findByID(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -50,6 +55,16 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findByID(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.sendStatus(404);
+    }
+    
+  });
+
 app.listen(PORT, () => {
-    console.log(`API server now on port 3${PORT}!`);
+    console.log(`API server now on port ${PORT}!`);
 });
